abf7785da7bdcc2bf3ca1bcf2152dfc4e903c5b9
Mohammed Ameen <jegroamin@gmail.com>
2018-07-25 12:02:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const returnLastTwoDrivers = function (drivers){
 
 let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
 
-const fareMultiplier = (function createFareMultiplier(){
+const fareMultiplier = function createFareMultiplier(){
   return ();
-})
+}
