b9128b7b973bb07dbe7d9113e7aea75581ebca7c
Mohammed Ameen <jegroamin@gmail.com>
2018-07-25 12:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const myVar = function returnFirstTwoDrivers (drivers){
+const returnFirstTwoDrivers = function (drivers){
   return newArray =[drivers[0], drivers[1]];
 }
 
